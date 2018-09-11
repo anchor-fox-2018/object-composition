4a6a8219d07383898c66c8a73e42438a78b64378
anastasia <anastasia.santoso.id@gmail.com>
2018-09-11 18:21:24 +0700

    Release 0 finished

--- composition.js
+++ composition.js
@@ -26,14 +26,40 @@ class ChocolateChip extends Cookie {
   }
 }
 
-class CookieFactory {
-  static create(options) {
-
+class OtherCookie extends Cookie {
+  constructor(name) {
+    super(name);
+    this._other_count = 150;
   }
 }
 
+class CookieFactory {
+  static create(options) {
+    let arrayKue = [];
+    for (var i = 0; i < options.length - 1; i++) {
+      let buatKueBaru = {};
+      if (options[i] === 'peanut butter') {
+        buatKueBaru = new PeanutButter(options[i]);
+      } else if (options[i] === 'chocolate chip') {
+        buatKueBaru = new ChocolateChip(options[i]);
+      } else {
+        buatKueBaru = new OtherCookie(options[i]);
+      }
+      arrayKue.push(buatKueBaru);
+    }
+    return arrayKue;
+  }
+}
+
+let fs = require('fs')
+let options = fs.readFileSync('cookies.txt')
+  .toString()
+  .split("\n")
+
 //TESTCASE
-let kueKacang = new PeanutButter('peanut butter')
-console.log(kueKacang);
-let kueCoklat = new ChocolateChip('chocolate chip')
-console.log(kueCoklat);
+// let kueKacang = new PeanutButter('peanut butter')
+// console.log(kueKacang);
+// let kueCoklat = new ChocolateChip('chocolate chip')
+// console.log(kueCoklat);
+let batch_of_cookies = CookieFactory.create(options);
+console.log(batch_of_cookies);
